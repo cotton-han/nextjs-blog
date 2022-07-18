38630fdd2801e9a02b5fb9b53993ff26fc62815c
cotton-han <cotton.dev96@gmail.com>
2022-07-18 21:13:46 +0900

    feat: Date 컴포넌트 구현

---
+++ components/date.js
@@ -0,0 +1,9 @@
+import { format, parseISO } from 'date-fns'; // 이건 조금 무거운 듯. 하지만 많이 사용됨
+
+export default function Date(props) {
+  const { dateString } = props;
+
+  const date = parseISO(dateString);
+
+  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
+}
